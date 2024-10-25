ce8fb8928d6734873185dc17b5f278f8e8d61ded
Kainy <kainykawai@gmail.com>
2024-10-25 12:18:37 +0300

    Refactor server2.js for improved routing and error handling

--- server2.js
+++ server2.js
@@ -5,37 +5,61 @@ const users = [
   { id: 2, name: "Jane Zainab" },
 ];
 
-const server = createServer((req, res) => {
-  if ((req.url === "/api/users") & (req.method === "GET")) {
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify(users));
-    res.end();
-}
-    else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-        const id = req.url.split("/")[3];
-        const user = users.find((user)=> user.id === parseInt(id));
-        if(user){
-            res.setHeader("Content-Type", "application/json");
-            res.write(JSON.stringify(user));
-            res.end();
-        }
-        else{
-            res.setHeader("Content-Type", "application/json");
-            res.statusCode = 404;
-            res.write(JSON.stringify({
-                message: "User not found",
-            }));
-            res.end();
-        }
-    }
-  else {
-    res.setHeader("Content-Type", "application/json");
+//logger middleware
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+//JSON middleware
+const jsonMiddleware = (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
+};
+const getUserbyIdHandler = (req, res) => {
+  const id = req.url.split("/")[3];
+  const user = users.find((user) => user.id === parseInt(id));
+  res.setHeader("Content-Type", "application/json");
+  if (user) {
+    res.write(JSON.stringify(user));
+  } else {
     res.statusCode = 404;
-    res.write(JSON.stringify({
-        message: "Route not found",
-    }));
-    res.end();
+    res.write(
+      JSON.stringify({
+        message: "User not found",
+      })
+    );
   }
+  next();
+};
+//Not Found Handler
+const notFoundHandler = (req, res) => {
+  res.statusCode = 404;
+  res.write(
+    JSON.stringify({
+      message: "Route not found",
+    })
+  );
+  res.end();
+};
+// Route Handler for GET /api/users
+const getUsersHandler = (req, res) => {
+  res.write(JSON.stringify(users));
+  res.end();
+};
+const server = createServer((req, res) => {
+  logger(req, res, () => {
+    jsonMiddleware(req, res, () => {
+      if (req.url === "/api/users" && req.method === "GET") {
+        getUsersHandler(req, res);
+      } else if (req.url.match(/\/api\/users\/\d+/) && req.method === "GET") {
+        getUserbyIdHandler(req, res);
+      } else {
+        notFoundHandler(req, res);
+      }
+    }
+    );
+  });
 });
 
 server.listen(PORT, () => {
